d71f8f7d2dad33743884a1de427428688c0d1ac3
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-01-03 15:45:27 -0800

    Testing use of arrows rather than vectors

--- scenes/test_rotation.py
+++ scenes/test_rotation.py
@@ -1,4 +1,16 @@
+import math
 from manim import *
+
+def distance_rotated(object, angle):
+    radius = object.side_length / 2
+    return angle * radius
+
+def distance_vector(object, angle, compliment):
+    radius = object.side_length / 2
+    displacement = compliment * radius
+    d_x = displacement * math.cos(angle)
+    d_y = displacement * math.sin(angle)
+    return Vector([d_x, -d_y, 0])
 
 class RotateSquare(Scene):
     def construct(self):
@@ -24,7 +36,7 @@ class RotateSquare(Scene):
         # Rotate square
         self.play(
             Rotate(square,
-                   about_point=(square.get_critical_point(UR)),
+                   about_point=(square.get_critical_point(DL)),
                    angle=-theta)
         )
         self.wait(reg_wait)
@@ -33,5 +45,16 @@ class RotateSquare(Scene):
         self.play(
             Create(cm_next)
         )
+
+        dist_displaced = distance_rotated(square, theta)
+        displacement_cm = Variable(dist_displaced,
+                                   MathTex(r'CM displacement'),
+                                   num_decimal_places=2
+        ).move_to([4,3,0])
+        displacement_vector = Arrow(start=[0,0,0], end=square.get_center(), buff=0)
+
+
+        self.play(Write(displacement_cm),
+                  Create(displacement_vector))
 
         self.wait(long_wait)
